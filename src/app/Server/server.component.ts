71354315cd5ea9dfdebc9a8edd4286d6a8ce9cf3
Hidayat arghandabi <hidayatarg@gmail.com>
2018-11-29 16:45:01 +0300

    string Interpolation

--- src/app/Server/server.component.ts
+++ src/app/Server/server.component.ts
@@ -1,15 +1,21 @@
 import { Component } from "@angular/core";
 
 
+
 @Component({
 selector: 'app-server',
-    template:`
-    <p>
-    The server component
-    </p>`,
+templateUrl: './server.component.html',
 styleUrls: ['./server.component.css']
 })
 
 export class ServerComponent{
-
+    /*Optional
+    serverId: number = 10;
+    sreverStatus: string = 'offline';
+    */
+    serverId= 10;
+    sreverStatus='offline';
+    getServiceStatus(){
+        return this.sreverStatus;
+    }
 }
