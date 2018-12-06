2f91cce8d7a4a3dbc7613534fb3eb78ade2b86c6
Hidayat arghandabi <hidayatarg@gmail.com>
2018-12-06 17:42:45 +0300

    Output of list of recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipes.mode';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,12 +10,17 @@ export class RecipeListComponent implements OnInit {
   // empty array define type as Recipe as an arry form
   recipes: Recipe[] = [
     new Recipe('Test Recipe', 'This is a test recipe',
+    'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg'),
+    new Recipe('Other Recipe', 'This is two test recipe',
     'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
   ];
+
 
   constructor() { }
 
   ngOnInit() {
+    
+    
   }
 
 }
