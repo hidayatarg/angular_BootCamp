41f705119340370bccbb777c408daf5dfa47621d
Hidayat arghandabi <hidayatarg@gmail.com>
2018-12-06 17:27:56 +0300

    adding content to the recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.mode';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  // empty array
-  recipes=[];
+  // empty array define type as Recipe as an arry form
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a test recipe',
+    'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg')
+  ];
 
   constructor() { }
 
