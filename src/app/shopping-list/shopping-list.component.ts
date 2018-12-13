a82420e9f512531b8d2dee81bc879a01bb8074e2
Hidayat arghandabi <hidayatarg@gmail.com>
2018-12-13 17:25:42 +0300

    Creating and outputting the shop list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
-  // array of ingridents
-  ingredients=[];
+  // array of ingridents of type model ingredients
+  // Creating ingredient
+  ingredients: Ingredient[]=[
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatos', 5)
+  ];
+
+  
+  
   constructor() { }
 
   ngOnInit() {
