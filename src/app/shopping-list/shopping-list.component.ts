403930d120c0ce4c88bcfdfb6eda11f567602eb6
Hidayat arghandabi <hidayatarg@gmail.com>
2018-12-13 17:15:43 +0300

    working with shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  // array of ingridents
+  ingredients=[];
   constructor() { }
 
   ngOnInit() {
